405c4a74f282311457a9f1e9092e31535acd737d
Kazzuki <madakazuki@icloud.com>
2020-07-04 17:29:14 +0900

    add function

--- tutorial/unit4/controlfow.py
+++ tutorial/unit4/controlfow.py
@@ -1,11 +1,24 @@
-x = int(input("Please enter an integer: "))
+while True:
+    x = int(input("整数を入力してください: "))
+    if x <= 10:
+        print("{}は10以下です".format(x))
+    elif 10<x and x<=25:
+        print("{}は10より大きく25以下です".format(x))
+    else:
+        print("{}は25より大きいです".format(x))
+        break
 
-if x < 0:
-    x = 0
-    print('Negative changed to zero')
-elif x == 0:
-    print('Zero')
-elif x == 1:
-        print('Single')
-else:
-        print('More')
+
+# s = input("整数を入力してください: ")
+# print(s)
+# print(type(s))
+
+# if x < 0:
+#     x = 0
+#     print('Negative changed to zero')
+# elif x == 0:
+#     print('Zero')
+# elif x == 1:
+#         print('Single')
+# else:
+#         print('More')
