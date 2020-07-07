63012b286d1e49548fdb51f1655899494d172fcb
Kazzuki <madakazuki@icloud.com>
2020-07-07 23:47:37 +0900

    p87 challenge

--- tutorial/unit4/container.py
+++ tutorial/unit4/container.py
@@ -1,2 +1,21 @@
-fruit = list()
-print(fruit)
+"""
+p87のチャレンジ問題
+・数学と同じで数式を書きながら理解する過程がプログラミンで全く身についていない。
+・はじめは、コードを書くことになれないかもしれないが、頭の中にある考えを表現していかないといつまで立ってもこのまま。
+・文章を書く、考えを書き出す習慣があるんだから、絶対コードを書くこともできる！俺はやれる。
+"""
+
+artists = list()
+artists = ["kazuki", "yui", "kome"]
+
+prace = ([139.8, "kanada"],[283.9, "hoge"],)
+
+kazuki_date = { "age":34,
+                "sex":"men",
+                "hoby":["bado","tyari","babanuki"]}
+
+print(artists)
+print(prace)
+print(kazuki_date)
+print(kazuki_date["age"])
+print(kazuki_date["hoby"][0])
---
+++ tutorial/unit4/string.py
@@ -0,0 +1,11 @@
+str = "カミュ"
+for i in str:
+    print(i)
+
+
+str1 = input("入力1:")
+str2 = input("入力2:")
+print("私は{}に{}をした".format(str1, str2))
+
+print("kazuki".capitalize())
+print("ka zu ki".split("k"))
