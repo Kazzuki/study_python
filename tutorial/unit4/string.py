54d15ac525f3563421ec0926f9ddcc844963886e
Kazzuki <madakazuki@icloud.com>
2020-07-08 23:59:47 +0900

    lkjfd

--- tutorial/unit4/string.py
+++ tutorial/unit4/string.py
@@ -9,3 +9,6 @@ print("私は{}に{}をした".format(str1, str2))
 
 print("kazuki".capitalize())
 print("ka zu ki".split("k"))
+
+test_list = ["The", "fox", "jumped", "over", "the", "fence", "."]
+new_list = test_list.join()
